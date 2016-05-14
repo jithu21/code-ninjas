b21ea72d5e57720a891688baffacb1527a9f8cdf
subhodeep <sdutta@gainsight.com>
2016-05-14 19:57:27 +0530

    code refactor

--- NextGenCRM/src/main/java/com/opinio/crm/entity/SatisfactionDayAgg.java
+++ NextGenCRM/src/main/java/com/opinio/crm/entity/SatisfactionDayAgg.java
@@ -1,7 +1,43 @@
 package com.opinio.crm.entity;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+
+import java.util.Date;
+
 /**
  * Created by Subhodeep on 14/05/16.
  */
 public class SatisfactionDayAgg {
+    @Id
+    private String id;
+    private String custId;
+    @Indexed
+    private Date orderDate;
+    @Indexed
+    private int customerRating;
+
+    public String getCustId() {
+        return custId;
+    }
+
+    public void setCustId(String custId) {
+        this.custId = custId;
+    }
+
+    public Date getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(Date orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public int getCustomerRating() {
+        return customerRating;
+    }
+
+    public void setCustomerRating(int customerRating) {
+        this.customerRating = customerRating;
+    }
 }
